5a228b31570f7230cb71ad63252098db01e39a19
Can Okan <tamadaminasordun@gmail.com>
2023-06-03 19:29:41 +0300

    mongodb database

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const { Client, IntentsBitField } = require('discord.js');
+const mongoose = require('mongoose');
 const eventHandler = require("./handlers/eventHandler");
+const logger = require('pino')();
 
 const client = new Client({
     intents: [
@@ -11,6 +13,17 @@ const client = new Client({
     ]
 });
 
-eventHandler(client);
+(async () => {
+    eventHandler(client);
+
+    mongoose.set('strictQuery', false);
+    await mongoose.connect(
+        process.env.MONGODB_URI
+    ).then(() => {
+        logger.info('successfully connected to mongodb cloud server');
+    }).catch((err) => {
+        logger.error(err);
+    });
+})();
 
 client.login(process.env.BOT_TOKEN);
